8195cdcdda804ab1122a20745b67e9fe650c7f67
GenericP3rson <genericp3rson@gmail.com>
2024-05-12 10:36:35 -0400

    pir functional

--- pir/pir.py
+++ pir/pir.py
@@ -1,31 +1,34 @@
-import numpy as np
+from sage.all import *
 
 class PIR:
-    def __init__(self, fhe):
+    def __init__(self, lhe):
         self.st = None
         self.lhe = lhe
+        self.A = None
+        self.q = lhe.q
 
-    def query(self, i, j, n):
-        uj = np.zeros((n,))
+    def query(self, i, j, sqrtN):
+        uj = matrix(GF(2), sqrtN, [0]*sqrtN)
         uj[j] = 1
 
-        sk = self.lhe.gen(n)
+        sk = self.lhe.gen()
 
-        qu = self.lhe.enc(sk, uj)
+        A, qu = self.lhe.enc(uj, sk)
+        self.A = A
         
         self.st = (sk, i)
 
         return qu, self.st
 
     def answer(self, D, qu):
-        ans = D * qu
+        ans = matrix(GF(self.q), D) * qu
 
         return ans
 
     def reconstruct(self, ans, st):
         sk, i = self.st
 
-        v = self.lhe.dec(sk, ans)
+        v = self.lhe.dec(self.A, ans, sk)
 
         return v[i]
 
